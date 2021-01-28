67f63d9202a99b7f9777a171e900abf617abc6c9
Hugh Rundle <hugh@hughrundle.net>
2021-01-28 18:57:43 +1100

    update github action
    - apparently it needs sudo with no password to npm install?
    - also added ability to run the json update action manually

--- .github/scripts/minify-map-json.sh
+++ .github/scripts/minify-map-json.sh
@@ -1,4 +1,4 @@
-npm install -g mapshaper
+sudo npm install -g mapshaper
 mapshaper ./boundaries.geo.json snap -clean -o force precision=0.0001 format=geojson ./boundaries.geo.json
-npm install -g topojson-server
+sudo npm install -g topojson-server
 geo2topo ./boundaries.geo.json -q 50000 > ./website/data/boundaries.topo.json
